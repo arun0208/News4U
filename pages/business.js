f8a7e213afff850a6d8ebf7907dbe3c41f7123f2
Arun <arunattri09@gmail.com>
2023-03-24 19:41:30 +0530

    added pagination to all pages

--- pages/business.js
+++ pages/business.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 import { Toolbar } from "@/components/Toolbar";
-
+import { useState,useEffect } from "react";
+import Pagination from '@/components/Pagination';
+import { paginate } from '@/components/Paginate';
 
 
 
@@ -13,34 +15,49 @@ export const getStaticProps = async ()=>{
   };
   };
   
-  export default function Home({news}) {
-    
-    return (
-      <>
-      <Toolbar />
-    
-      <div class="container mx-auto">
-        <h1>Business News</h1>
-        <div class="card-group" >
-          <div class="row">
-        {news.articles.map((ne) => {
-          return (
-            
-              <div class="card col-4">
-                <img class="card-img-top" src={ne.urlToImage} alt="Card image cap" />
-                <div class="card-body">
-                  <h5 class="card-title">{ne?.title}</h5>
-                  <p class="card-text">{ne?.description}</p>
-                  <a href={ne.url} target='_blank' class="btn btn-primary">{ne.source.name}</a>
-                </div>
+  export default function Home({ news }) {
+    const [currentPage, setCurrentPage] = useState(1);
+    const pageSize = 10;
+   
+    const onPageChange = (page) => {
+      setCurrentPage(page);
+      window.scrollTo(0,0);
+    };
+    const paginatedPosts = paginate(news.articles, currentPage, pageSize);
+  
+      return (
+        <>
+          <Toolbar />
+  
+          <div class="container mx-auto">
+            <h1 class="text-center">Business News</h1>
+            <div class="card-group" >
+              <div class="row">
+                {paginatedPosts.map((ne) => {
+                  return (
+  
+                    <div class="card col-4">
+                      <img class="card-img-top" src={ne.urlToImage} alt="Card image cap" />
+                      <div class="card-body">
+                        <h5 class="card-title">{ne?.title}</h5>
+                        <p class="card-text">{ne?.description}</p>
+                        <a href={ne.url} target='_blank' class="btn btn-primary">{ne.source.name}</a>
+                      </div>
+                    </div>
+  
+  
+                  )
+                })}
               </div>
-            
-
-          )
-        })}
-        </div>
-        </div>
-      </div>
-      </>
-    )
-  }
+            </div>
+          </div>
+          <Pagination
+         items={news.totalResults} 
+         currentPage={currentPage} 
+         pageSize={pageSize} 
+         onPageChange={onPageChange}
+          />
+          
+        </>
+      );
+    };
